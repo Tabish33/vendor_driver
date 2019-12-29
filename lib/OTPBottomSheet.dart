b8f38c762a4d21e34ad379363fc826e9bed7ff72
unknown <tabishbashir@gmail.com>
2019-12-30 05:25:50 +0530

    genertate and upload pdf for vendor delivery mail

--- lib/OTPBottomSheet.dart
+++ lib/OTPBottomSheet.dart
@@ -1,5 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:printing/printing.dart';
+import 'package:pdf/pdf.dart';
+import 'dart:io';
+import 'package:pdf/widgets.dart' as pdfLib;
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -80,6 +86,9 @@ class _OTPBottomSheetState extends State<OTPBottomSheet> {
     String order_id =  order['id'].toString();
     String otp = order_id.substring(9,13);
 
+    
+  
+
     if(otp_controller.text == otp) {
        showCupertinoModalPopup(
          context: context,
@@ -100,7 +109,10 @@ class _OTPBottomSheetState extends State<OTPBottomSheet> {
           }
        ); 
 
-       await storeDb.collection("vendor_orders").document(order_id).updateData({"status":"completed"});
+       File pdf = await createPdf(order);
+       String url = await uploadPdf(pdf, order['id']);
+       await storeDb.collection("vendor_orders").document(order_id).updateData({"status":"completed", 'invoice_url': url});
+
        CloudFunctions.instance.getHttpsCallable(
          functionName: "sendVendorDeliveryMail",
        ).call(
@@ -120,6 +132,71 @@ class _OTPBottomSheetState extends State<OTPBottomSheet> {
        
     }
     else print("wrong");
+  }
+
+  createPdf(Map order)async{
+    final pdfLib.Document pdf = pdfLib.Document();
+
+    pdf.addPage(
+      pdfLib.Page(
+        pageFormat: PdfPageFormat.a5,
+        build: (context) {
+          return pdfLib.Container(
+            child: pdfLib.Column(
+              crossAxisAlignment: pdfLib.CrossAxisAlignment.start,
+              children: [
+                pdfLib.Text("relllo",style: pdfLib.TextStyle( fontSize: 20.0, fontWeight: pdfLib.FontWeight.bold)),
+                pdfLib.Padding(padding: pdfLib.EdgeInsets.only(bottom: 5.0)),
+                pdfLib.Text("Purchase Order", style: pdfLib.TextStyle( fontSize: 30.0, fontWeight: pdfLib.FontWeight.bold)),
+                pdfLib.Padding(padding: pdfLib.EdgeInsets.only(top: 5.0)),
+                pdfLib.Row(
+                  children: [
+                    pdfLib.Text("Ship Date",style: pdfLib.TextStyle( fontSize: 13.0, fontWeight: pdfLib.FontWeight.bold)),
+                    pdfLib.Padding(padding: pdfLib.EdgeInsets.only(left: 15.0)),
+                    pdfLib.Text("Ship Via",style: pdfLib.TextStyle( fontSize: 13.0, fontWeight: pdfLib.FontWeight.bold)),
+                    pdfLib.Padding(padding: pdfLib.EdgeInsets.only(left: 35.0 )),
+                    pdfLib.Text("Terms",style: pdfLib.TextStyle( fontSize: 13.0, fontWeight: pdfLib.FontWeight.bold)),
+                  ]
+                ),
+                pdfLib.Padding(padding: pdfLib.EdgeInsets.only(top: 3.0)),
+                pdfLib.Row(
+                  children: [
+                    pdfLib.Text("${order['date']}",style: pdfLib.TextStyle( fontSize: 11.0, fontWeight: pdfLib.FontWeight.bold)),
+                    pdfLib.Padding(padding: pdfLib.EdgeInsets.only(left: 20.0)),
+                    pdfLib.Text("relllo",style: pdfLib.TextStyle( fontSize: 11.0, fontWeight: pdfLib.FontWeight.bold)),
+                    pdfLib.Padding(padding: pdfLib.EdgeInsets.only(left: 60.0 )),
+                    pdfLib.Text("Shippin and Payment Terms",style: pdfLib.TextStyle( fontSize: 11.0, fontWeight: pdfLib.FontWeight.bold)),
+                  ]
+                ),
+                pdfLib.Padding(padding: pdfLib.EdgeInsets.only(top: 10.0 , bottom: 15.0)),
+                pdfLib.Table.fromTextArray(context: context, data: <List<String>>[
+                <String>['Item', 'Qty', 'Unit Price', 'Total Price'],
+                ...order['items'].map(
+                    (item) => ["${item['data']['name']}", "${item['quantity']} ${item['data']['unit']}","${item['data']['price_per_qty']}" , "${item['data']['price_per_qty']*item['quantity']}" ]),
+                ["","","Subtotal","${order['bill'] - 30}" ],
+                ["","","Shipping","30" ],
+                ["","","Total","${order['bill']}" ]
+              ]),
+              ]
+            )
+          );
+        }
+      )
+    );
+
+    final String dir = (await getApplicationDocumentsDirectory()).path;
+    final String path = '$dir/example.pdf';
+    final File file = File(path);
+    return await file.writeAsBytes(pdf.save());
+  }
+
+  uploadPdf(File file,id)async{
+    StorageReference storageReference = FirebaseStorage.instance.ref().child("vendor_invoices/${id}");    
+    StorageUploadTask uploadTask = storageReference.putFile(file);    
+    await uploadTask.onComplete;   
+    print('File Uploaded');    
+    String url = await storageReference.getDownloadURL();  
+    return url;
   }
 
   showSuccessDialog(){
